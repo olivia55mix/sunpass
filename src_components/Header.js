8fd2bd302f2079194d07a9607e88e6bde5b63b3a
olivia55mix <olivia55mix@outlook.com>
2026-07-09 12:19:09 +0530

    Create Header.js

---
+++ src_components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="app-header">
+      <div>
+        <h1>SunPass Expense Control Portal</h1>
+      </div>
+      <div className="tagline">Official Travel Reconciliation Utility</div>
+    </header>
+  );
+}
+
+export default Header;
